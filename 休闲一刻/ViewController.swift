844e1f05fce728c959e4d6b174086ac90429dc39
5201-mac <a5201@7-5.local>
2017-06-07 11:03:59 +0800

    Initial Commit

---
+++ 休闲一刻/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  休闲一刻
+//
+//  Created by 5201-mac on 2017/6/7.
+//  Copyright © 2017年 5201-mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
